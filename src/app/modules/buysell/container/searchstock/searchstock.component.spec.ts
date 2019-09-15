c8f19f9bcefdee9363587148ae862eb1d7ac2105
Bhargavi Patel <bhargavipatel259@gmail.com>
2019-09-15 14:01:28 -0400

    component changes

--- src/app/modules/buysell/container/searchstock/searchstock.component.spec.ts
+++ src/app/modules/buysell/container/searchstock/searchstock.component.spec.ts
@@ -84,4 +84,17 @@ describe('SearchstockComponent', () => {
   });
 });
 
+describe("SearchstockService", () => {
+  let SearchstockService: SearchstockService;
 
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [SearchstockService]
+    });
+    SearchstockService = TestBed.get(SearchstockService);
+  });
+
+  it("should be created", () => {
+    expect(SearchstockService).toBeTruthy();
+  });
+});
